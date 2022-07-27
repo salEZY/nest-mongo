950129e5221fa66ad6abdb587537659979d3abed
salEZY <bobicaleks@yahoo.com>
2022-07-27 17:33:09 +0200

    start project

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/auth-nest-db', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
