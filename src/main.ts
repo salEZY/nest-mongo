981e430c2f101031150dff87c0f6b5f5cbf036b7
salEZY <bobicaleks@yahoo.com>
2022-08-23 17:16:04 +0200

    start adding swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  // Swagger
+  const options = new DocumentBuilder().setTitle('Nest Mongo practice').setDescription('MONGO JWT').setVersion('1.0').addTag('Users').build()
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(3000);
 }
 bootstrap();
